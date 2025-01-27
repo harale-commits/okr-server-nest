399f37dd56f66635a8f655cb55a4ae0ef2bacb33
harale-commits <harale150@gmail.com>
2025-01-27 17:44:10 +0530

    Initialize nest backend

---
+++ src/hello-world/hello-world.controller.ts
@@ -0,0 +1,17 @@
+import {Controller, Get} from '@nestjs/common';
+import {HelloWorldService} from "./hello-world.service";
+
+@Controller('hello-world')
+export class HelloWorldController {
+
+
+  constructor(private helloWorldService : HelloWorldService) {
+  }
+
+  @Get('/')
+  show() {
+
+    return this.helloWorldService.show();
+  }
+
+}
---
+++ src/hello-world/hello-world.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@nestjs/common';
+
+@Injectable()
+export class HelloWorldService {
+
+  show() {
+    return "hello world";
+  }
+
+}
